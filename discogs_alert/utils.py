c5a06eabcac936a0d2e937b420e36d7442ca8837
Michael Ball <michael.ball@oculid.com>
2021-03-11 11:23:18 +0100

    made condition dict better

--- discogs_alert/utils.py
+++ discogs_alert/utils.py
@@ -1,10 +1,10 @@
 CONDITIONS = {
-    'Poor (P)': 0,
-    'Fair (F)': 1,
-    'Good (G)': 2,
-    'Good Plus (G+)': 3,
-    'Very Good (VG)': 4,
-    'Very Good Plus (VG+)': 5,
-    'Near Mint (NM or M-)': 6,
-    'Mint (M)': 7,
+    'P': 0,
+    'F': 1,
+    'G': 2,
+    'G+': 3,
+    'VG': 4,
+    'VG+': 5,
+    'NM': 6,
+    'M': 7,
 }
