6686b8ef48b88c42f6418b2e03b09eff3a0e153e
michael <michael_ball@brown.edu>
2021-03-14 22:15:14 +0100

    one more conversion dict

--- discogs_alert/utils.py
+++ discogs_alert/utils.py
@@ -8,3 +8,14 @@ CONDITIONS = {
     'NM': 6,
     'M': 7,
 }
+
+CONDITION_SHORT = {
+    'Poor (P)': 'P',
+    'Fair (F)': 'F',
+    'Good (G)': 'G',
+    'Good Plus (G+)': 'G+',
+    'Very Good (VG)': 'VG',
+    'Very Good Plus (VG+)': 'VG+',
+    'Near Mint (NM or M-)': 'NM',
+    'Mint (M)': 'M',
+}
