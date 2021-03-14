ad3d907127ad5e010d2b64ce6522ab6051eb2c77
michael <michael_ball@brown.edu>
2021-03-14 22:15:28 +0100

    minor update

--- discogs_alert/notify.py
+++ discogs_alert/notify.py
@@ -4,12 +4,12 @@ import requests
 __all__ = ['send_pushbullet_push']
 
 
-def send_pushbullet_push(token, message_body, message_title):
+def send_pushbullet_push(token, message_title, message_body):
     """ Sends notification of found record via pushbullet.
 
-    @param: (str) pushbullet token needed to send notification to correct user
-    @param message_body: (str) body of the message
+    @param token: (str) pushbullet token needed to send notification to correct user
     @param message_title: (str) title of message
+    @param message_body: (str) body of the message
     :return: True if successful, False otherwise.
     """
 
@@ -18,7 +18,7 @@ def send_pushbullet_push(token, message_body, message_title):
         headers = {'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json'}
         resp = requests.post('https://api.pushbullet.com/v2/pushes', data=json.dumps(message), headers=headers)
         if resp.status_code != 200:
-            print("ERROR")
+            print(f"error {resp.status_code} sending pushbullet notification: {resp.content}")
             return False
         else:
             return True
