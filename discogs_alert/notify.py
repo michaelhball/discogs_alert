4ba4a64cf6382d61aaaa9d8631932d636de3c92c
michael <michael_ball@brown.edu>
2021-03-15 09:24:02 +0100

    updated notify to prevent re-sending of identical pushes

--- discogs_alert/notify.py
+++ discogs_alert/notify.py
@@ -14,14 +14,27 @@ def send_pushbullet_push(token, message_title, message_body):
     """
 
     try:
-        message = {"type": "note", "title": message_title, "body": message_body}
         headers = {'Authorization': 'Bearer ' + token, 'Content-Type': 'application/json'}
-        resp = requests.post('https://api.pushbullet.com/v2/pushes', data=json.dumps(message), headers=headers)
-        if resp.status_code != 200:
-            print(f"error {resp.status_code} sending pushbullet notification: {resp.content}")
-            return False
-        else:
-            return True
+        message = {"type": "note", "title": message_title, "body": message_body}
+        url = 'https://api.pushbullet.com/v2/pushes'
+
+        # work out if there is an existing, identical push
+        resp = requests.get(url, headers=headers)
+        have_already_sent = False
+        for p in resp.json().get('pushes'):
+            if p.get('title') == message.get('title') and p.get('body') == message.get('body'):
+                have_already_sent = True
+                break
+
+        # if not, send one
+        if not have_already_sent:
+            resp = requests.post(url, data=json.dumps(message), headers=headers)
+            if resp.status_code != 200:
+                print(f"error {resp.status_code} sending pushbullet notification: {resp.content}")
+                return False
+            else:
+                return True
+        return True
     except Exception as e:
         print(f"Exception sending pushbullet push: {e}")
         return False
