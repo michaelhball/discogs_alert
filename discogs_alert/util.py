d2706cad8e6942e1394cb4c4b9bfcdecf2f92d39
Michael Ball <michael.h.s.ball@gmail.com>
2022-02-18 09:28:55 +0100

    added conditions_satisfied helper function

--- discogs_alert/util.py
+++ discogs_alert/util.py
@@ -1,6 +1,64 @@
 import functools
 import time
 
+from discogs_alert import types as da_types
+
+
+def conditions_satisfied(
+    listing: da_types.Listing,
+    release: da_types.Release,
+    seller_filters: da_types.SellerFilters,
+    record_filters: da_types.RecordFilters,
+):
+    """Validates that a given listing satisfies all conditions and filters, including both global filters
+    (set via environment variables or via the CLI at runtime) and per-release filters (set in wantlist.json).
+
+    Args:
+        listing: the listing to validate
+        release: the release object, defined by the user, corresponding to the given listing
+        seller_filters: the global seller filters
+        record_filters: the global record (media & sleeve condition) filters
+
+    Returns: True if the given listing satisfies all conditions, False otherwise.
+    """
+
+    # verify seller conditions
+    if (
+        listing.seller_avg_rating is not None
+        and seller_filters.min_seller_rating is not None
+        and listing.seller_avg_rating < seller_filters.min_seller_rating
+    ):
+        return False
+    if (
+        seller_filters.min_seller_sales is not None
+        and listing.seller_num_ratings is not None
+        and listing.seller_num_ratings < seller_filters.min_seller_sales
+    ):
+        return False
+
+    # verify media condition, either globally or for this specific release
+    if listing.media_condition < (release.min_media_condition or record_filters.min_media_condition):
+        return False
+
+    # optionally verify all sleeve conditions, either globally or for this specific release
+    # NB: we have to check all conditions before we know whether they're satisfied
+    we_good = (listing.sleeve_condition == da_types.CONDITION.GENERIC) and (
+        release.accept_generic_sleeve or record_filters.accept_generic_sleeve
+    )
+    we_good = we_good or (
+        (listing.sleeve_condition == da_types.CONDITION.NO_COVER)
+        and (release.accept_no_sleeve or record_filters.accept_no_sleeve)
+    )
+    we_good = we_good or (
+        (listing.sleeve_condition == da_types.CONDITION.NOT_GRADED)
+        and (release.accept_ungraded_sleeve and record_filters.accept_ungraded_sleeve)
+    )
+    we_good = we_good or (
+        listing.sleeve_condition < (release.min_sleeve_condition or record_filters.min_sleeve_condition)
+    )
+
+    return we_good
+
 
 def time_cache(seconds: int, maxsize=None, typed=False):
     """Least-recently-used cache decorator with time-based cache invalidation.
