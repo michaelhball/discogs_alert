c70d6623837799ac363cb4c1af4c2fd1527230fd
ddelange <14880945+ddelange@users.noreply.github.com>
2022-12-09 10:14:24 +0100

    Leftover

--- discogs_alert/__main__.py
+++ discogs_alert/__main__.py
@@ -212,6 +212,8 @@ def main(
     )
 
     da_loop.loop(*args)
+    if test:
+        return
     schedule.every(int(60 / frequency)).minutes.do(lambda: da_loop.loop(*args))
     while 1:
         schedule.run_pending()
