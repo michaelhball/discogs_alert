ed9e335d6dc9368adcfbe4189178784941abe812
mhsb <michael.h.s.ball@gmail.com>
2023-06-04 11:42:16 +0200

    Adds `RequiredIf` click option subclass (#55)

--- discogs_alert/util/click.py
+++ discogs_alert/util/click.py
@@ -1,3 +1,5 @@
+from typing import Any, List, Mapping, Tuple
+
 import click
 
 
@@ -11,17 +13,46 @@ class NotRequiredIf(click.Option):
         assert self.not_required_if, "'not_required_if' parameter required"
         helper_text = f" NOTE: This argument is mutually exclusive with {self.not_required_if}"
         kwargs["help"] = (kwargs.get("help", "") + helper_text).strip()
-        super(NotRequiredIf, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
-    def handle_parse_result(self, ctx, opts, args):
+    def handle_parse_result(
+        self, ctx: click.Context, opts: Mapping[str, Any], args: List[str]
+    ) -> Tuple[Any, List[str]]:
         we_are_present = self.name in opts
         other_present = self.not_required_if in opts
         if other_present:
             if we_are_present:
                 raise click.UsageError(
-                    "Illegal usage: `%s` is mutually exclusive with `%s`" % (self.name, self.not_required_if)
+                    f"Illegal usage: `{self.name}` is mutually exclusive with `{self.not_required_if}`"
                 )
             else:
                 self.prompt = None
 
-        return super(NotRequiredIf, self).handle_parse_result(ctx, opts, args)
+        return super().handle_parse_result(ctx, opts, args)
+
+
+class RequiredIf(click.Option):
+    """A class enabling Click to require a specific argument if some boolean condition is met. Both `required_if`
+    and `required_if_str` must be specified for any Click option using RequiredIf, the former a callable that
+    determines whether the option is required, and the latter a string used to explain the condition (see usage below).
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.required_if = kwargs.pop("required_if")
+        self.required_if_str = kwargs.pop("required_if_str")
+        assert (
+            self.required_if is not None and self.required_if_str is not None
+        ), "Both the `required_if` and the `required_if_str` parameters are required if using the `RequiredIf` class"
+        super().__init__(*args, **kwargs)
+
+    def handle_parse_result(
+        self, ctx: click.Context, opts: Mapping[str, Any], args: List[str]
+    ) -> Tuple[Any, List[str]]:
+        """If the `required_if` condition is met, check whether this option's value is set before proceeding."""
+
+        value, _ = self.consume_value(ctx, opts)
+        if self.required_if(ctx.params) and value is None:
+            raise click.UsageError(f"Illegal usage: `{self.name}` is required when `{self.required_if_str}`")
+
+        self.prompt = None
+        return super().handle_parse_result(ctx, opts, args)
