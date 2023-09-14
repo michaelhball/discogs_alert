810361570c62eff4085849cf8572a07a0578f3d4
ddelange <14880945+ddelange@users.noreply.github.com>
2023-09-14 11:15:40 +0200

    Support webdriver-manager 4.0.0

--- discogs_alert/client.py
+++ discogs_alert/client.py
@@ -16,7 +16,7 @@ from selenium import webdriver
 from selenium.webdriver.chromium.options import ChromiumOptions
 from selenium.webdriver.chromium.service import ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.utils import ChromeType
 
 from discogs_alert import entities as da_entities, scrape as da_scrape
 
