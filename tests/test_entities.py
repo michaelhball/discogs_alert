fddf60ebbc37640e9f4d90cd288329ba1763e8f4
Michael Ball <michael.h.s.ball@gmail.com>
2024-04-03 19:56:21 +0200

    Fix more stuff

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -9,23 +9,22 @@ def test_conditions_satisfied():
 
 
 def test_convert_listing_price_currency(mock_currency_rates, rates: da_currency.CurrencyRates):
-
     # full listing price
     lp1 = da_entities.ListingPrice("GBP", 10, shipping=da_entities.ShippingPrice("GBP", 5))
     lp1_target = da_entities.ListingPrice(
-        "EUR", 10 / rates["EURGBP"], shipping=da_entities.ShippingPrice("EUR", 5 / rates["EURGBP"])
+        "EUR", 10 / rates["GBP"], shipping=da_entities.ShippingPrice("EUR", 5 / rates["GBP"])
     )
     assert lp1.convert_currency("EUR") == lp1_target
 
     # no shipping
     lp2 = da_entities.ListingPrice("GBP", 10, shipping=None)
-    lp2_target = da_entities.ListingPrice("EUR", 10 / rates["EURGBP"], shipping=None)
+    lp2_target = da_entities.ListingPrice("EUR", 10 / rates["GBP"], shipping=None)
     assert lp2.convert_currency("EUR") == lp2_target
 
     # different currencies
     lp3 = da_entities.ListingPrice("GBP", 10, shipping=da_entities.ShippingPrice("AUD", 5))
     lp3_target = da_entities.ListingPrice(
-        "EUR", 10 / rates["EURGBP"], shipping=da_entities.ShippingPrice("EUR", 5 / rates["EURAUD"])
+        "EUR", 10 / rates["GBP"], shipping=da_entities.ShippingPrice("EUR", 5 / rates["AUD"])
     )
     assert lp3.convert_currency("EUR") == lp3_target
 
